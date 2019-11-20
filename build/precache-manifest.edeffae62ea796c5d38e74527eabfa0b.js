36834940ae5f78cd4aef2614da49516a21f18682
Ravid Bondy <ravid135@gmail.com>
2019-11-20 18:09:14 +0200

    npm run build

---
+++ build/precache-manifest.edeffae62ea796c5d38e74527eabfa0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9f4c46308567ca16d15ce69481c63e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cf62c3c79495680417d",
+    "url": "/static/css/main.1c9c8f9a.chunk.css"
+  },
+  {
+    "revision": "bd20bbf459cec751bdde",
+    "url": "/static/js/2.353f94bf.chunk.js"
+  },
+  {
+    "revision": "6cf62c3c79495680417d",
+    "url": "/static/js/main.1ea3a8cc.chunk.js"
+  },
+  {
+    "revision": "17a4fc25b31712ae5ff8",
+    "url": "/static/js/runtime-main.20fc6019.js"
+  }
+]);
